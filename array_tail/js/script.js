f0849af296580366e23aafc6052428f281c9fe05
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-10-15 16:41:55 +0200

    add const

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -3,3 +3,16 @@
     //creiamo l'utility di numeri casuali
     //chiediamo all'utente quanti numeri vuole 
     //stampiamo gli ultimi 5 numeri
+
+
+//prendiamo il value delle caselle input
+const nElement = document.getElementById('n-element').value;
+const nPrintElement = document.getElementById('n-print-element').value;
+const button = document.querySelector('button');
+
+
+
+//CONSOLE LOG
+console.log(nElement);
+console.log(nPrintElement);
+console.log(button);
