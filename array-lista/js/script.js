690282b158b127ece6d9bd6c3f636c4a703c41c6
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-10-15 15:48:15 +0200

    add element to array with push

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -1,3 +1,10 @@
+//PASSAGGI ARRAY-LISTA
+    //creiamo un array con la lista della frutta che è in frigo
+    //aggiungiamo la pesca con push all'array
+    //con ciclo for verifichiamo che ci sia il cocomero
+    //stampare frase sul cocomero
+
+//creiamo array
 const fruits = [
     'banana', 
     'mela', 
@@ -9,3 +16,11 @@ const fruits = [
     'limone', 
     'fragola'  
 ];
+//vediamo l'array sul console log
+console.log(fruits);
+
+//aggiungiamo la pesca all'array
+fruits.push('pesca');
+//verifichiamo se la pesca è stata inserita
+console.log(fruits);
+
