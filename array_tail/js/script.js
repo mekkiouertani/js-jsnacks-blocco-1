8e37833867f4f7b9fd0d645617f81c4ebed859b1
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-10-15 18:05:41 +0200

    updated cicle for, add innerHTML

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -8,22 +8,26 @@
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
-
 //prendiamo il value delle caselle input
 
-const nPrintElement = document.getElementById('n-print-element');
+
 const button = document.querySelector('button');
 const casualNumbers = [];
 
 button.addEventListener('click', function(){
+
     let nElement = document.getElementById('n-element').value;
     console.log(nElement.value);
 
-    for (let i = 0; i < nElement; i++){
-        casualNumbers.push(getRndInteger(1, 100));
+    let nPrintElement = document.getElementById('n-print-element');
+    console.log(nPrintElement.value);
+
+    for (let i = 0; i <= nElement; i++){
+        casualNumbers.push(getRndInteger(1,100));
         console.log(casualNumbers);
-        document.body.innerHTML += casualNumbers; 
-    }
-})
+    } 
+    document.body.innerHTML += `gli ultimi 5 numeri: ${casualNumbers.slice(-5)}`; 
+});
+
 
 
