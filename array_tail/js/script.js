f6dd0f971fb340eea37800b888256454344230d0
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-10-15 23:18:27 +0200

    remove changelog to array tail js

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -9,22 +9,15 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 //prendiamo il value delle caselle input
-
-
 const button = document.querySelector('button');
 const casualNumbers = [];
 
 button.addEventListener('click', function(){
-
     let nElement = document.getElementById('n-element').value;
-    console.log(nElement.value);
-
     let nPrintElement = document.getElementById('n-print-element');
-    console.log(nPrintElement.value);
-
+    
     for (let i = 0; i <= nElement; i++){
         casualNumbers.push(getRndInteger(1,100));
-        console.log(casualNumbers);
     } 
     document.body.innerHTML += `gli ultimi 5 numeri: ${casualNumbers.slice(-5)}`; 
 });
